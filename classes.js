8b0b8c1cd45a6c5c16f5ddb797f8d3332fda62d9
Amit Thakur <amitthakur.softprodigy@gmail.com>
2022-05-26 14:42:16 +0530

    classes done!

---
+++ classes.js
@@ -0,0 +1,16 @@
+class User{
+    constructor(name,email,city){
+        this.name=name;
+        this.email=email;
+        this.city=city;
+    }
+    printData(){
+        console.log(`Name: ${this.name} Email: ${this.email}  City: ${this.city}`)
+    }
+}
+
+const User1=new User("Amit","example1@gmail.com","nagar")
+User1.printData()
+
+const User2=new User("Aditya","example2@gmail.com","pune")
+User2.printData()
